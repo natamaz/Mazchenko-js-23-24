caf8eadc723a3b01404c30cd529eca837c44d23c
natamaz <sidorova87@mail.ru>
2016-05-31 09:38:10 +0300

    1

---
+++ js/controller.js
@@ -0,0 +1,53 @@
+﻿define('controller',['jquery'],function($){
+	
+	function Controller(model, view){
+		var self = this;
+		
+		view.elements.addBtn.on('click', addItem);
+		view.elements.listContainer.on('click', '.item-delete',removeItem);
+		view.elements.listContainer.on('click','.item-show', editItem);
+		
+		function addItem(){
+			var newItem = view.elements.input.val();
+			if(newItem.length === 0){
+				return;
+			} else {
+			model.addItem(newItem);
+			view.renderList(model.data);
+			view.elements.input.val('');
+			}
+		}
+		
+		function removeItem(){
+			var item = $(this).attr('data-value');
+			
+			model.removeItem(item);
+			view.renderList(model.data);
+		}
+		
+		function editItem() {
+			var item = $(this).attr('data-show');
+			var index = $('.item-show').index(this);
+			var editItem = $('.item-show').eq(index);
+			var editItemInput = $('.item-edit').eq(index);
+			var newItem = editItem.html();
+	
+			editItemInput.val(editItem.html());
+			editItem.hide();
+			editItemInput.show();
+			editItemInput.select();
+	
+			editItemInput.focusout(function(){
+			editItemInput.hide();
+			editItem.html(editItemInput.val());
+			editItem.show();
+			newItem = editItem.html();
+			
+			model.editItem(item, newItem);
+			view.renderList(model.data);
+			});
+		};
+		
+	}
+	return Controller;
+});
---
+++ js/script.js
@@ -0,0 +1,27 @@
+﻿requirejs.config({
+	paths: {
+		'jquery': 'jquery.min',
+		'tmpl': 'tmpl',
+	},
+	shim:{
+		'jquery': {
+			exports: 'jquery.min'
+		},
+		'tmpl': {
+			exports: 'tmpl'
+		}
+	}
+});
+
+require(
+	[
+	'jquery','tmpl','model','view','controller'
+	],
+function($, tmpl, Model, View, Controller){
+	$(function (){
+	var firstToDoList =['learn Java Script', 'learn Angular JS', 'make portfolio'];
+	var model = new Model(firstToDoList);
+	var view = new View(model);
+	var controller = new Controller(model, view);
+	})	
+});
